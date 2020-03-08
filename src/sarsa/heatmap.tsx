54492ad7e9e224a28df60d7ad3e3c7b56d564578
CharlyCst <charly.castes@epfl.ch>
2020-03-08 11:27:40 +0100

    Improved heatmap

--- src/sarsa/heatmap.tsx
+++ src/sarsa/heatmap.tsx
@@ -8,14 +8,34 @@ import Agent from "./agent";
 const color1 = [255, 0, 255];
 const color2 = [0, 255, 255];
 
-const scale = 0.8;
-
 const getColor = (q: number) => {
-    const percent = Math.min(1, Math.max(0, 0.5 + scale * q));
+    const percent = Math.min(1, Math.max(0, 1 / (1 + Math.exp(-3 * q))));
     return `rgb(
         ${Math.floor(color2[0] + (color1[0] - color2[0]) * percent)},
         ${Math.floor(color2[1] + (color1[1] - color2[1]) * percent)},
         ${Math.floor(color2[2] + (color1[2] - color2[2]) * percent)})`;
+};
+
+const drawScale = (ctx: CanvasRenderingContext2D, w: number, h: number) => {
+    ctx.strokeStyle = "#000000";
+    const gradient = ctx.createLinearGradient(
+        0,
+        (0.5 + h) * blockSize,
+        w * blockSize,
+        (0.9 + h) * blockSize
+    );
+    gradient.addColorStop(1, `rgb(${color1[0]},${color1[1]},${color1[2]})`);
+    gradient.addColorStop(0, `rgb(${color2[0]},${color2[1]},${color2[2]})`);
+    ctx.fillStyle = gradient;
+    ctx.strokeRect(0, (0.5 + h) * blockSize, w * blockSize, 0.4 * blockSize);
+    ctx.fillRect(0, (0.5 + h) * blockSize, w * blockSize, 0.4 * blockSize);
+
+    ctx.font = "14px sans-serif";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("0", (w * blockSize) / 2, (1.5 + h) * blockSize);
+    ctx.fillText("+∞", w * blockSize, (1.5 + h) * blockSize);
+    ctx.fillText("-∞", 0, (1.5 + h) * blockSize);
 };
 
 const drawQValues = (
@@ -28,23 +48,37 @@ const drawQValues = (
     ctx.lineWidth = 1;
     for (let i = 0; i < w; i++) {
         for (let j = 0; j < h; j++) {
-            let q = 0;
-            for (const val of Q[i][j]) {
-                q += val;
-            }
-            q = q / 4;
-
-            ctx.fillStyle = getColor(q);
+            ctx.fillStyle = getColor(0);
             ctx.fillRect(i * blockSize, j * blockSize, blockSize, blockSize);
+            ctx.strokeRect(i * blockSize, j * blockSize, blockSize, blockSize);
 
-            const x =
-                (Q[i][j][RIGHT] - Q[i][j][LEFT]) * scale * blockSize * 0.5;
-            const y = (Q[i][j][BOTTOM] - Q[i][j][UP]) * scale * blockSize * 0.5;
-
+            ctx.fillStyle = getColor(Q[i][j][UP]);
             ctx.beginPath();
             ctx.moveTo((i + 0.5) * blockSize, (j + 0.5) * blockSize);
-            ctx.lineTo((i + 0.5) * blockSize + x, (j + 0.5) * blockSize + y);
-            ctx.stroke();
+            ctx.lineTo((i + 1) * blockSize, j * blockSize);
+            ctx.lineTo(i * blockSize, j * blockSize);
+            ctx.fill();
+
+            ctx.fillStyle = getColor(Q[i][j][RIGHT]);
+            ctx.beginPath();
+            ctx.moveTo((i + 0.5) * blockSize, (j + 0.5) * blockSize);
+            ctx.lineTo((i + 1) * blockSize, j * blockSize);
+            ctx.lineTo((i + 1) * blockSize, (j + 1) * blockSize);
+            ctx.fill();
+
+            ctx.fillStyle = getColor(Q[i][j][BOTTOM]);
+            ctx.beginPath();
+            ctx.moveTo((i + 0.5) * blockSize, (j + 0.5) * blockSize);
+            ctx.lineTo((i + 1) * blockSize, (j + 1) * blockSize);
+            ctx.lineTo(i * blockSize, (j + 1) * blockSize);
+            ctx.fill();
+
+            ctx.fillStyle = getColor(Q[i][j][LEFT]);
+            ctx.beginPath();
+            ctx.moveTo((i + 0.5) * blockSize, (j + 0.5) * blockSize);
+            ctx.lineTo(i * blockSize, (j + 1) * blockSize);
+            ctx.lineTo(i * blockSize, j * blockSize);
+            ctx.fill();
         }
     }
 };
@@ -59,6 +93,8 @@ export const HeatMap = (props: { map: Map; agent: Agent }) => {
         if (ctx == null) return;
 
         ctx.translate(blockSize, blockSize);
+
+        drawScale(ctx, props.map.w, props.map.h);
         while (true) {
             drawQValues(ctx, props.agent.Q, props.map.w, props.map.h);
             await sleep(200);
